daa80758fbf54384d0d14e2c405df3fe8dfecde6
AMIT SINGH GAUR <amitgour59@gmail.com>
2021-09-08 20:28:55 +0530

    Update getPython.sh

--- scripts/getPython.sh
+++ scripts/getPython.sh
@@ -5,5 +5,5 @@ wget https://www.python.org/ftp/python/3.9.7/Python-3.9.7.tar.xz
 tar -xf Python-3.9.7.tar.xz
 cd Python-3.9.7
 ./configure
-make altinstall
+sudo make altinstall
 python3.9 --version
