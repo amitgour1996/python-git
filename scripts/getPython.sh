109497d4eed4d1c71916c8fa56dea1a92fc29aaf
AMIT SINGH GAUR <amitgour59@gmail.com>
2021-09-08 18:07:01 +0530

    Update getPython.sh

--- scripts/getPython.sh
+++ scripts/getPython.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt-get install zlib1g-dev -y
+#apt-get install zlib1g-dev -y
 wget https://www.python.org/ftp/python/3.9.7/Python-3.9.7.tar.xz
 tar -xf Python-3.9.7.tar.xz
 cd Python-3.9.7
